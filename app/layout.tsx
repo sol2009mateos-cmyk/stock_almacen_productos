8b7a898a8f578c0939c37692b43c6c24f03e60d7
sol2009mateos-cmyk <sol2009mateos@gmail.com>
2026-07-14 20:13:56 -0300

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,11 @@
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="es-AR">
+      <body>{children}</body>
+    </html>
+  );
+}
